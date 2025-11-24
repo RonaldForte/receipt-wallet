8f9c55c7708f454dd2d2a020ee40ca5dda89eedc
Ronald Forte Jr. <ronaldjrforte@gmail.com>
2025-11-23 19:20:33 -0500

    Finished CRUD RecieptService + test in App.js

--- src/main/java/com/receiptwallet/service/ReceiptService.java
+++ src/main/java/com/receiptwallet/service/ReceiptService.java
@@ -33,7 +33,7 @@ public class ReceiptService {
 }
 
   public void addReceipt(Receipt r){
-    String sql = "INSERT INTO receipts (storeName, amount, date) VALUES (?, ?, ?)"; //prepared sql statement (no SQL injection)
+    String sql = "INSERT INTO receipts (storeName, amount, date) VALUES (?, ?, ?)";
     try (Connection conn = DriverManager.getConnection(url, user, password);
     PreparedStatement ps = conn.prepareStatement(sql)) {
     ps.setString(1, r.getStoreName());
@@ -63,5 +63,76 @@ public class ReceiptService {
       e.printStackTrace();
     }
     return list;
+  }
+
+  public void deleteReceipt(int id){
+    String sql = "DELETE FROM receipts WHERE id = ?";
+    try (Connection conn = DriverManager.getConnection(url, user, password);
+      PreparedStatement ps = conn.prepareStatement(sql)) {
+        ps.setInt(1, id);
+        
+        ps.executeUpdate();
+      } catch (SQLException e){
+        e.printStackTrace();
+      }
+  }
+
+  public List<Receipt> searchByStoreName(String storeName){
+    List<Receipt> list = new ArrayList<>();
+    String sql = "SELECT storeName, amount, date FROM receipts WHERE storeName = ?";
+    try (Connection conn = DriverManager.getConnection(url, user, password);
+    PreparedStatement ps = conn.prepareStatement(sql)) {
+      ps.setString(1, storeName);
+      
+      ResultSet rs = ps.executeQuery();
+
+      while(rs.next()) {
+        String store = rs.getString("storeName");
+        double amount = rs.getDouble("amount");
+        String date = rs.getString("date");
+
+        list.add(new Receipt(store, amount, date));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return list;
+  }
+
+  public List<Receipt> searchByDate(String date){
+    List<Receipt> list = new ArrayList<>();
+    String sql = "SELECT storeName, amount, date FROM receipts WHERE date = ?";
+    try (Connection conn = DriverManager.getConnection(url, user, password);
+    PreparedStatement ps = conn.prepareStatement(sql)) {
+      ps.setString(1, date);
+
+      ResultSet rs = ps.executeQuery();
+
+      while(rs.next()){
+        String store = rs.getString("storeName");
+        double amount = rs.getDouble("amount");
+        String recieptDate = rs.getString("date");
+
+        list.add(new Receipt(store, amount, recieptDate));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return list;
+  }
+
+  public void updateReceipt(int id, String newStore, double newAmount, String newDate) {
+    String sql = "UPDATE receipts SET storeName = ?, amount = ?, date = ? WHERE id = ?";
+    try (Connection conn = DriverManager.getConnection(url, user, password);
+    PreparedStatement ps = conn.prepareStatement(sql)) {
+    ps.setString(1, newStore);
+    ps.setDouble(2, newAmount);
+    ps.setString(3, newDate);
+    ps.setInt(4, id);
+
+    ps.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 }
